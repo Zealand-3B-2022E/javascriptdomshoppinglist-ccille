a939a9580f5dcf0c136f6ad30d46c34f16fe3f5f
Cecilie Maria Hernig <ceci7542@edu.easj.dk>
2022-10-26 12:35:48 +0200

    clickhidden function

--- js/index.js
+++ js/index.js
@@ -1,2 +1,71 @@
 //Write your Javascript code here
 console.log("Shoppinglist")
+
+let myElement = document.getElementById("2")
+console.log(myElement)
+
+let myList = document.getElementsByTagName("li")
+console.log(myList)
+
+myElement.setAttribute("class", "healthy")
+
+//Attribute hidden
+//myElement.setAttribute("hidden", "true")
+
+//All with class healthy
+let myHealthy = document.getElementsByClassName("healthy")
+console.log(myHealthy)
+
+//Parent
+let p = document.getElementById("0").parentNode
+console.log(p)
+
+//Add new li element to list
+let newElement = document.createElement("li")
+let newtextelement = document.createTextNode("Cake")
+newElement.setAttribute("class", "unhealthy")
+newElement.setAttribute("id", "7")
+newElement.append(newtextelement)
+document.getElementById("list").append(newElement)
+
+let newList = document.createElement("li")
+let newtext = document.createTextNode
+
+//Function tilføjer element til listen unhealty
+function addgoods(e)
+{
+    console.log("In the add goods function");
+    let inputText = (elementInput).value;
+    console.log("The input text is: " + inputText);
+
+    let listUnhealthy = document.getElementById("listUnhealthy")
+    let newli = document.createElement("li");
+    let newtext = document.createTextNode(inputText)
+    newli.appendChild(newtext)
+    newli.setAttribute("class", "unhealthy")
+
+    listUnhealthy.appendChild(newli);
+}
+
+//Find Input
+let elementInput = document.getElementById("newgoods")
+
+//Add event
+document.getElementById("addbutton").addEventListener("click",  addgoods)
+
+//Function to hide element
+let elementsli = document.getElementById("list").addEventListener("click", ClickHidden)
+
+function ClickHidden(e)
+{
+    let clickElement = (e.target)
+
+    if (e.target && (clickElement).nodeName == "LI")
+    {
+        console.log(clickElement.id + " was clicked")
+        clickElement.setAttribute("hidden", "true")
+    }
+}
+
+
+
